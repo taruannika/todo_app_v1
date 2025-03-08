127e599952b9972175e708f3effc3a4666a7e5af
HamalainenTaru <thamalainen1990@gmail.com>
2025-03-08 22:13:02 +0200

    created router and pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,30 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { AddTodo, AllTodos, Home, Stats, Error } from "./pages";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <AddTodo />,
+      },
+      {
+        path: "all-todos",
+        element: <AllTodos />,
+      },
+      {
+        path: "stats",
+        element: <Stats />,
+      },
+    ],
+  },
+]);
+
 const App = () => {
-  return <div>App</div>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
---
+++ client/src/pages/index.js
@@ -0,0 +1,5 @@
+export { default as Home } from "./Home";
+export { default as Error } from "./Error";
+export { default as AddTodo } from "./AddTodo";
+export { default as Stats } from "./Stats";
+export { default as AllTodos } from "./AllTodos";
